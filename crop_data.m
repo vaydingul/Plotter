415a91636bf913a1af81eb269f8651c6b14eaab6
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-08-05 17:02:55 +0300

    Finalizing the first version

--- crop_data.m
+++ crop_data.m
@@ -1,10 +1,10 @@
-function data_cropped = crop_data(data)
+function data_cropped = crop_data(data, dim)
 %MEAN_UNEQUAL_LENGTH_DATA Summary of this function goes here
 %   Detailed explanation goes here
 
-length_array = cellfun(@length, data);
+length_array = cellfun(@(c) length(c{dim}), data);
 minimum_length = min(length_array);
-data_cropped = cell2mat(cellfun(@(c) c(1:minimum_length), data, 'UniformOutput', false));
+data_cropped = cell2mat(cellfun(@(c) c{dim}(1:minimum_length), data, 'UniformOutput', false));
 
 end
 
